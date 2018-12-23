3d1bf7842c154800149fba517a392c1e81d6f97f
Plamen Yordanov <plamen_ydv@yahoo.co.uk>
2018-12-23 01:08:15 +0000

    pagination for lists in backend

---
+++ Web/MovieDatabase.Web/Areas/Admin/Controllers/Base/EntityListController.cs
@@ -0,0 +1,30 @@
+﻿namespace MovieDatabase.Web.Areas.Admin.Controllers.Base
+{
+    using System;
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using MovieDatabase.Common;
+    using MovieDatabase.Web.Areas.Admin.Models;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    [Area("Admin")]
+    public class EntityListController : Controller
+    {
+        protected IQueryable<T> PaginateList<T>(PaginationVM pagination, IQueryable<T> query)
+        {
+            var skip = (pagination.Page - 1) * pagination.PageSize;
+            var take = pagination.PageSize;
+
+            return query.Skip(skip).Take(take);
+        }
+
+        protected int GetTotalPages(int pageSize, int entityCount)
+        {
+            var totalPages = (int)Math.Ceiling(decimal.Divide(entityCount, pageSize));
+
+            return totalPages;
+        }
+    }
+}
---
+++ Web/MovieDatabase.Web/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿namespace MovieDatabase.Web.Areas.Admin.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using MovieDatabase.Common;
+    using MovieDatabase.Data.Models;
+    using MovieDatabase.Services.Contracts;
+    using MovieDatabase.Services.Identity;
+    using MovieDatabase.Web.Areas.Admin.Controllers.Base;
+    using MovieDatabase.Web.Areas.Admin.Models;
+    using MovieDatabase.Web.Areas.Admin.Models.Users;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    [Area("Admin")]
+    public class UsersController : EntityListController
+    {
+        private ApplicationUserManager<ApplicationUser> userManager;
+
+        public UsersController(ApplicationUserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index(PaginationVM pagination)
+        {
+            string currentId = this.userManager.GetUserId(this.User);
+            var usersQuery = this.userManager.Users.Where(u => u.Id != currentId).ProjectTo<UserVM>();
+
+            var paginatedUsers = this.PaginateList<UserVM>(pagination, usersQuery).ToList();
+
+            int totalPages = this.GetTotalPages(pagination.PageSize, usersQuery.Count());
+
+            return this.View(new UserListVM
+            {
+                Users = paginatedUsers,
+                NextPage = pagination.Page < totalPages ? pagination.Page + 1 : pagination.Page,
+                PreviousPage = pagination.Page > 1 ? pagination.Page - 1 : pagination.Page,
+                CurrentPage = pagination.Page,
+                TotalPages = totalPages,
+                ShowPagination = totalPages > 1,
+            });
+        }
+    }
+}
